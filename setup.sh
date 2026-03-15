935cb3cf104babda2f244a57f86143a9de7eea9b
MrUNKNOWNN7 <83583700+MrUNKNOWNN7@users.noreply.github.com>
2026-03-15 14:00:48 +0530

    Update setup script

--- setup.sh
+++ setup.sh
@@ -10,25 +10,28 @@ rm -rf kernel/oneplus/sm8450
 rm -rf kernel/oneplus/sm8450-modules
 rm -rf kernel/oneplus/sm8450-devicetrees
 rm -rf hardware/oplus
-rm -rf hardware/qcom-caf/sm8450
-rm -rf hardware/pixelworks/interfaces
 
 #device-tree
-git clone https://github.com/asmodeus-oos/android_device_realme_ferrari.git -b lineage-23.0 device/realme/ferrari
-git clone https://github.com/asmodeus-oos/android_device_oneplus_sm8450-common.git -b lineage-23.0 device/oneplus/sm8450-common
+git clone https://github.com/asmodeus-oos/android_device_realme_ferrari.git -b lineage-23.2 device/realme/ferrari
+git clone https://github.com/asmodeus-oos/android_device_oneplus_sm8450-common.git -b lineage-23.2 device/oneplus/sm8450-common
 
 #vendor
-git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_realme_ferrari.git -b lineage-23.0 vendor/realme/ferrari
-git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_oneplus_sm8450-common.git -b lineage-23.0 vendor/oneplus/sm8450-common
+git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_realme_ferrari.git -b lineage-23.2 vendor/realme/ferrari
+git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_oneplus_sm8450-common.git -b lineage-23.2 vendor/oneplus/sm8450-common
 
 #kernel
-git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450.git -b lineage-23.0 kernel/oneplus/sm8450
-git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-modules.git -b lineage-23.1 kernel/oneplus/sm8450-modules
-git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-devicetrees.git -b lineage-23.0 kernel/oneplus/sm8450-devicetrees
+git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450.git -b lineage-23.2 kernel/oneplus/sm8450
+git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-modules.git -b lineage-23.2 kernel/oneplus/sm8450-modules
+git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-devicetrees.git -b lineage-23.2 kernel/oneplus/sm8450-devicetrees
 
 #hardware
-git clone https://github.com/asmodeus-oos/android_hardware_oplus.git -b lineage-23.0 hardware/oplus
-git clone https://github.com/asmodeus-oos/hardware_pixelworks_interfaces.git -b lineage-22.2 hardware/pixelworks/interfaces
- 
-#Hals
-git clone https://github.com/asmodeus-oos/android_hardware_qcom-caf_sm8450.git hardware/qcom-caf/sm8450
+git clone https://github.com/asmodeus-oos/android_hardware_oplus.git -b lineage-23.2 hardware/oplus
+
+#Patches
+cd hardware/qcom-caf/sm8450/audio/agm
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_agm refs/changes/20/417220/3 && git cherry-pick FETCH_HEAD
+cd ../../display
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/30/432430/9 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/31/432431/12 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/32/432432/10 && git cherry-pick FETCH_HEAD
+cd ../../../../
