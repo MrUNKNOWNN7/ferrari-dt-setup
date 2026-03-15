e30271c000004ed5fea477cb1f49bc3abe03c458
MrUNKNOWNN7 <83583700+MrUNKNOWNN7@users.noreply.github.com>
2026-03-15 15:48:13 +0530

    update setup.sh for self use.

--- setup.sh
+++ setup.sh
@@ -12,17 +12,17 @@ rm -rf kernel/oneplus/sm8450-devicetrees
 rm -rf hardware/oplus
 
 #device-tree
-git clone https://github.com/asmodeus-oos/android_device_realme_ferrari.git -b lineage-23.2 device/realme/ferrari
-git clone https://github.com/asmodeus-oos/android_device_oneplus_sm8450-common.git -b lineage-23.2 device/oneplus/sm8450-common
+git clone https://github.com/MrUNKNOWNN7/android_device_realme_ferrari.git -b lineage-23.2 device/realme/ferrari
+git clone https://github.com/MrUNKNOWNN7/android_device_oneplus_sm8450-common.git -b lineage-23.2 device/oneplus/sm8450-common
 
 #vendor
 git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_realme_ferrari.git -b lineage-23.2 vendor/realme/ferrari
 git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_oneplus_sm8450-common.git -b lineage-23.2 vendor/oneplus/sm8450-common
 
 #kernel
-git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450.git -b lineage-23.2 kernel/oneplus/sm8450
-git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-modules.git -b lineage-23.2 kernel/oneplus/sm8450-modules
-git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-devicetrees.git -b lineage-23.2 kernel/oneplus/sm8450-devicetrees
+git clone https://github.com/MrUNKNOWNN7/android_kernel_oneplus_sm8450.git -b lineage-23.2 kernel/oneplus/sm8450
+git clone https://github.com/MrUNKNOWNN7/android_kernel_oneplus_sm8450-modules.git -b lineage-23.2 kernel/oneplus/sm8450-modules
+git clone https://github.com/MrUNKNOWNN7/android_kernel_oneplus_sm8450-devicetrees.git -b lineage-23.2 kernel/oneplus/sm8450-devicetrees
 
 #hardware
 git clone https://github.com/asmodeus-oos/android_hardware_oplus.git -b lineage-23.2 hardware/oplus
